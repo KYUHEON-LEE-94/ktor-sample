e6dc2958f4e33bf9bb07bbd30f8be6cfe7bc625c
khlee <panpan68@naver.com>
2024-11-25 14:52:09 +0900

    stockDetail.js에 상세화면 UI 추가

--- ktor-stock/stock-front/src/App.js
+++ ktor-stock/stock-front/src/App.js
@@ -1,4 +1,4 @@
-// import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Stock from './pages/Stock';
 import StockDetail from './pages/StockDetail';
@@ -9,7 +9,7 @@ function App() {
         <Routes>
           {/* 라우트 설정 */}
           <Route path="/" element={<Stock />} />
-          <Route path="/stock-detail" component={StockDetail} />
+          <Route path="/stock-detail" element={<StockDetail />} />
         </Routes>
     </Router>
   );
--- ktor-stock/stock-front/src/pages/StockDetail.js
+++ ktor-stock/stock-front/src/pages/StockDetail.js
@@ -4,17 +4,27 @@ import { useLocation } from 'react-router-dom';
 function StockDetail() {
     const location = useLocation();
     const stockRequestVo = location.state || {}; // stockRequestVo가 없을 경우 빈 객체로 초기화
-    console.log("테스트 : ", stockRequestVo)
+
     return (
-      <div>
-        <h1>주식 상세 정보</h1>
-        {Object.keys(stockRequestVo).length > 0 ? ( // stockRequestVo가 비어있지 않을 때만 렌더링
-          <pre>{JSON.stringify(stockRequestVo, null, 2)}</pre> // stockRequestVo를 표시
-        ) : (
-          <p>주식 정보가 없습니다.</p> // 주식 정보가 없을 경우 메시지 표시
-        )}
-        {/* 추가적인 상세 정보를 여기에 표시할 수 있습니다 */}
-      </div>
+        <div className="p-4 bg-gray-100 min-h-screen">
+            <h1 className="text-2xl font-bold text-center mb-4">주식 상세 정보</h1>
+            {Object.keys(stockRequestVo).length > 0 ? (
+                <div className="bg-white shadow-md rounded-lg p-6">
+                    <h2 className="text-xl font-semibold mb-2">기업명: {stockRequestVo.itmsNm}</h2>
+                    <p className="text-gray-700"><strong>시장 구분:</strong> {stockRequestVo.mrktCls}</p>
+                    <p className="text-gray-700"><strong>ISIN 코드:</strong> {stockRequestVo.isinCd}</p>
+                    <p className="text-gray-700"><strong>시가:</strong> {stockRequestVo.mkp}</p>
+                    <p className="text-gray-700"><strong>최고가:</strong> {stockRequestVo.hipr}</p>
+                    <p className="text-gray-700"><strong>저가:</strong> {stockRequestVo.lopr}</p>
+                    <p className="text-gray-700"><strong>거래량:</strong> {stockRequestVo.trqu}</p>
+                    <p className="text-gray-700"><strong>거래대금:</strong> {stockRequestVo.trPrc}</p>
+                    <p className="text-gray-700"><strong>상장일:</strong> {stockRequestVo.lstgDt}</p>
+                    {/* 추가적인 정보가 있다면 여기에 추가 */}
+                </div>
+            ) : (
+                <p className="text-center text-red-500">주식 정보가 없습니다.</p>
+            )}
+        </div>
     );
 }
 
