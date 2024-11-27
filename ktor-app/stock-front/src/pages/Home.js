9c8bd956cb617f016e16d06737a97fc049182b35
khlee <panpan68@naver.com>
2024-11-27 10:11:20 +0900

    ktor-stock -> ktor-app으로 이동

--- ktor-app/stock-front/src/pages/Home.js
+++ ktor-app/stock-front/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Dashboard from '../components/Dashboard'
 
 function Home() {
     return (
         <div className="p-4 bg-gray-100 min-h-screen flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold mb-4">WelCome My App</h1>
+            <Dashboard />
         </div>
     );
 }
--- ktor-stock/README.md
+++
@@ -1,2 +0,0 @@
-# 출처
-- https://ktor.io/docs/server-create-websocket-application.html#improve-design
